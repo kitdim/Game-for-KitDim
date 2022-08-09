316283d22181d857f96ccbf9e2331068cda356d8
Kit <kitaykin9808@gmail.com>
2022-08-09 17:16:54 +0300

    0.0

---
+++ alien_invasion/alien_invasion.py
@@ -0,0 +1,63 @@
+import sys
+import pygame
+from settings import Settings
+from ship import Ship
+
+class AlienInvasion:
+
+    def __init__(self):
+        """Инициализация игры и создание игровых ресурсов"""
+        pygame.init()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("ВОЙНА")
+        self.screen = pygame.display.set_mode((1600, 900), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
+        self.ship = Ship(self)
+        
+    def run_game(self):
+        """Запуск основного цикла игры"""
+        while True:
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+            
+    def _check_events(self):
+        """Проверка нажатия"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """Реагирует на нажатие клавиш."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+
+    def _check_keyup_events(self, event):
+        """Реагирует на отпускание клавиш."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+        elif event.key == pygame.K_q:
+            sys.exit()    
+    
+    def _update_screen(self):
+        """Обновления экрана"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()  
+                  
+if __name__ == '__main__':
+    #Создание экземпляра и запуск игры
+    ai = AlienInvasion()
+    ai.run_game()
