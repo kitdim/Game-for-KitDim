9eeafeacf6d010aab9cf8dc2c989a9062e6b980b
Kit <kitaykin9808@gmail.com>
2022-08-17 13:09:48 +0300

    modified:   alien_invasion/__pycache__/alien.cpython-310.pyc 	modified:   alien_invasion/__pycache__/bullet.cpython-310.pyc 	new file:   alien_invasion/__pycache__/button.cpython-310.pyc 	new file:   alien_invasion/__pycache__/game_stats.cpython-310.pyc 	new file:   alien_invasion/__pycache__/scoreboard.cpython-310.pyc 	modified:   alien_invasion/__pycache__/settings.cpython-310.pyc 	modified:   alien_invasion/__pycache__/ship.cpython-310.pyc 	new file:   alien_invasion/button.py 	modified:   alien_invasion/game_stats.py 	new file:   alien_invasion/scoreboard.py 	modified:   alien_invasion/settings.py 	modified:   alien_invasion/ship.py

--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -1,16 +1,19 @@
 import pygame
  
-class Ship:
+from pygame.sprite import Sprite
+ 
+class Ship(Sprite):
     """A class to manage the ship."""
  
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position."""
+        super().__init__()
         self.screen = ai_game.screen
         self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Load the ship image and get its rect.
-        self.image = pygame.image.load('c:/Users/kitdim/Desktop/MyProgram/alien_invasion/images/ship.bmp')
+        self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
 
         # Start each new ship at the bottom center of the screen.
